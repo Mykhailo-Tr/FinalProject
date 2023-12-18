4daa1cea50474247d77e89c0bedd60f698d067dc
Mykhailo Tretiak <mykhailo.tretiak098@gmail.com>
2023-12-18 17:43:46 +0200

    Added SQL_db.py (work with db), edit pages

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, render_template, url_for, request
+from SQL_db import DataBase
 
 
 app = Flask(__name__)
+db = DataBase('database.db')
 
 
 @app.route("/index")
@@ -12,49 +14,55 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template('about/index.html', page='about')
+    return render_template('about/index.html', page='about', title='Про Ліцей - РОГАТИНСЬКИЙ ЛІЦЕЙ №1')
 
 
 @app.route("/contacts")
 def contacts():
-    return render_template('contact/contact.html', page='contacts')
+    return render_template('contact/contact.html', page='contacts', title='Контакти - РОГАТИНСЬКИЙ ЛІЦЕЙ №1')
 
 
 @app.route("/sign-in", methods=["GET", "POST"])
 def sign_in():
     if request.method == "GET":
-        return render_template('auth/sign_in.html')
+        return render_template('auth/sign_in.html', title='Увійти - РОГАТИНСЬКИЙ ЛІЦЕЙ №1')
     elif request.method == "POST":
-        return render_template('auth/sign_in.html')
+        
+        return render_template('auth/sign_in.html', title='Увійти - РОГАТИНСЬКИЙ ЛІЦЕЙ №1')
     
 
 @app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     if request.method == "GET":
-        return render_template('auth/sign_up.html')
+        return render_template('auth/sign_up.html', title='Зареєструватися - РОГАТИНСЬКИЙ ЛІЦЕЙ №1')
     elif request.method == "POST":
-        return render_template('auth/sign_up.html')
-    
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        db.add_user(username, email, password)
+        
+        users = db.get_users()
+        return f"{users}"  
     
     
 @app.route("/heros")
 def heros():
-    return render_template("main/geroi.html", page='index')
+    return render_template("main/geroi.html", page='index', title='Герої не вмирають! - РОГАТИНСЬКИЙ ЛІЦЕЙ №1')
 
 
 @app.route("/about/history")
 def history():
-    return render_template('about/history.html', page='about')
+    return render_template('about/history.html', page='about', title='Історія Ліцею - РОГАТИНСЬКИЙ ЛІЦЕЙ №1')
 
 
 @app.route("/about/olympiads")
 def olympiads():
-    return render_template('about/olymp.html', page='about')
+    return render_template('about/olymp.html', page='about', title='Олімпіади та конкурси - РОГАТИНСЬКИЙ ЛІЦЕЙ №1')
 
 
 @app.route("/about/symbols")
 def symbols():
-    return render_template('about/symbol.html', page='about')
+    return render_template('about/symbol.html', page='about', title='Символіка Ліцею - РОГАТИНСЬКИЙ ЛІЦЕЙ №1')
     
 
 
