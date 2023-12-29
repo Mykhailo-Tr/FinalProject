aef9c3d26497dd9da124cf6e91f585a959a74f9f
Mykhailo Tretiak <mykhailo.tretiak098@gmail.com>
2023-12-29 20:33:19 +0200

    Make DataBase class Singleton

--- SQL_db.py
+++ SQL_db.py
@@ -3,52 +3,20 @@ import config
 
 
 class DataBase:
-    obj = None
+    _instance = None
     
-
+    def __new__(cls, db_filename: str):
+        if cls._instance is None:
+            cls._instance = super(DataBase, cls).__new__(cls)
+        return cls._instance
+    
+    
     def __init__(self, db_filename: str):
         self.filename = db_filename
         self.connect = sqlite3.connect(db_filename, check_same_thread=False)
         self.cursor = self.connect.cursor()
         
-    # def __new__(cls, *args, **kwargs):
-    #     if cls.obj is None:
-    #         cls.obj = super().__new__(cls, cls.filename , *args, **kwargs)
-    #     return cls.obj
-        
-    def add_user(self, username: str, email: str, password: str) -> None:
-        with self.connect:
-            user = self.cursor.execute("SELECT 1 FROM users WHERE username == '{key}'".format(key=username)).fetchone()
-            if not user:
-                role = "admin" if username in config.ADMINS_USERNAMES else "user"
-                
-                self.cursor.execute("""INSERT INTO users (username, email, password, role) VALUES(?, ?, ?, ?)""",
-                                    (username, email, password, role))
-
-                self.connect.commit()
-                
-    def get_user(self, user_id: int):
-        with self.connect:
-            return self.cursor.execute("""SELECT * FROM users WHERE user_id=(?)""", [user_id]).fetchall()
-        
-    def get_users(self):
-        with self.connect:
-            return self.cursor.execute("""SELECT * FROM users ORDER BY role DESC, username ASC""").fetchall()
-        
-    def get_user_id(self, username: str):
-        with self.connect:
-            return self.cursor.execute('''SELECT id FROM users WHERE username=(?)''', [username]).fetchone()
-        
-    def delete_user(self, user_id: int):
-        with self.connect as conn:
-            self.cursor.execute('DELETE FROM users WHERE id=(?)', [user_id])
-            conn.commit()
-            
-    def get_email(self, username: str):
-        with self.connect as conn:
-            return self.cursor.execute('''SELECT email FROM users WHERE username=(?)''', [username]).fetchone()
-
-
+    
     def get_news(self):
         """Get all news from database."""
         with self.connect:
@@ -109,3 +77,10 @@ class DataBase:
         with self.connect as connect:
             self.cursor.execute('DELETE FROM olympiads WHERE id = ?', (post_id,))
             connect.commit()
+            
+            
+            
+if __name__ == '__main__':
+    o1 = DataBase('database.db')
+    o2 = DataBase('database.db')
+    print(o1 is o2)
