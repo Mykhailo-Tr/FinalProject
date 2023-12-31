c6b4901802f2c57630e9e922d2051c3989c18c39
Mykhailo Tretiak <mykhailo.tretiak098@gmail.com>
2023-12-31 21:00:27 +0200

    added README and LICENSE

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from logger import logger
 from SQL_db import DataBase
 from JSON_db import JsonDatabase
 from flask_sqlalchemy import SQLAlchemy
-from config import SQLALCHEMY_DATABASE, UPLOAD_FOLDER, DATABASE_FILE, JSON_FILE
+from config import SQLALCHEMY_DATABASE, UPLOAD_FOLDER, DATABASE_FILE, JSON_FILE, SECRET_KEY
 
 app = Flask(__name__)
-app.logger = logger
+app.config["SECRET_KEY"] = SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config["SECRET_KEY"] = "abc"
+app.logger = logger
 site_db = DataBase(DATABASE_FILE)
 db = SQLAlchemy()
 titles = JsonDatabase(JSON_FILE)
